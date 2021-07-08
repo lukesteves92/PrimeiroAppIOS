0b52ad2e1e41176e390793391f8be076a2c35124
Lucas Esteves <lucas.coelho9280@gmail.com>
2021-07-08 13:51:28 -0300

    first commit

---
+++ PrimeiroApp/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  PrimeiroApp
+//
+//  Created by Lucas Esteves on 22/03/17.
+//  Copyright © 2017 Lucas Esteves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        //exibir uma mensagem
+        print("Meu primeiro APP")
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
